01376e85030cbb60a6ee8d2b5e0a30c1d211e8bf
Clebson Santos <67807818+ClebTech@users.noreply.github.com>
2024-12-08 13:03:59 -0300

    Update script.js

--- simple-form/scripts/script.js
+++ simple-form/scripts/script.js
@@ -1,26 +1,25 @@
 function myFunction(){
-    const form = document.querySelector('form');
-    const dados = document.querySelector('#dados');
+    const formu = document.querySelector('form'); //estou selecionando o meu formulario pela tag <form>
+    const dados = document.querySelector('#dados'); //estou selecioanando a div com id 'dados', nesta div serão mostrados os dados na tela
 
 
-    function recebeEventoFrom (evento){
-        evento.preventDefault();
+    function recebeEventoFrom (evento){ //função que recebe o evento de enviar o formulario, ou seja, quando o usuario clica para enviar o formulario
+        evento.preventDefault(); //evitar que a pagina recarregue e os dados desapareçam
 
-        const nome = form.querySelector('#nome');
-        const email = form.querySelector('#email');
-        const idade = form.querySelector('#idade');
+        const nome = formu.querySelector('#nome'); //guardando o dado do input que tem id 'nome' dentro de uma constante chamada 'nome'
+        const email = formu.querySelector('#email'); //guardando o dado do input que tem id 'email' dentro de uma constante chamada 'email'
+        const idade = formu.querySelector('#idade'); //guardando o dado do input que tem id 'idade' dentro de uma constante chamada 'idade'
 
-        const pessoas = [];
+        const pessoas = []; //criando um array que armazenará os objetos 'pessoa'
 
-        pessoas.push({nome: nome.value, 
+        pessoas.push({nome: nome.value,     //adicionando no array um objeto 'pessoa', estou pegando o valor que esta na const criada previamente
                     email: email.value, 
                     idade: idade.value});
 
-        console.log(pessoas);
-        dados.innerHTML += `<p>${nome.value}, ${email.value}, ${idade.value}</p>`
+        dados.innerHTML += `<p>${nome.value}, ${email.value}, ${idade.value}</p>`;  //mostrando os dados do usuario na tela, atraves da div 'dados'
     }
 
-    form.addEventListener('submit', recebeEventoFrom);
+    formu.addEventListener('submit', recebeEventoFrom); //dentro da tag <form> há um botão de submit, aqui estou dizendo que quando esse botão de submit que esta dentro da tag <form> for clicado, a função 'recebeEventoFrom' deverá ser executada... eu não coloco paranteses na função, pois quero que a função seja executada apenas quando o botão submit for clicado, caso eu coloque parenteses, a função será executada imediatamente, mesmo se o usuario nao clicar em submit e isso resultaria em um erro. Perceba que, quando eu ponho parenteses na frente da função eu estou passando o resultado que função retorna e nao a função em si, quando eu não coloco parenteses, eu estou passando a propria função como referencia
 }
 
 myFunction();
